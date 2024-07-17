2a133ce77cdc05b2d39bf68dd86762f86eae4faa
juhanikat <juhani.kataja@gmail.com>
2024-07-17 11:23:29 +0300

    connecting roads are now merged together

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseButton
-from shapely.geometry import LineString, Point, Polygon, box
+from shapely.geometry import LineString, Point, Polygon, box, MultiLineString
 from shapely.ops import nearest_points
-from shapely import intersection, equals, snap
+from shapely import intersection, equals, snap, line_merge
 from shapely.geometry.multipoint import MultiPoint
 import mplcursors
 """
@@ -26,6 +26,8 @@ distance = point_a.distance(point_b)
 print(f"Distance between points A and B: {distance}")
 """
 
+print_click_info = False  # use to print information on each mouse click
+
 hb_size = 0.3
 normal_p_color = "b"
 selected_p_color = "r"
@@ -43,9 +45,9 @@ class Counter:
     def __init__(self):
         self.count = 1
 
-    def __call__(self):
+    def __call__(self, amount=1):
         val = self.count
-        self.count += 1
+        self.count += amount
         return val
 
 
@@ -59,7 +61,9 @@ class Hmmm:
         self.points = []  # Points
         self.roads = []  # LineStrings
         self.current_road_points = []  # Points
+        self.current_road_connects_to = None
         self.plotted_points = {}  # (Point: plotted point) pairs
+        self.plotted_lines = {}  # (LineString: plotted Line2D) pairs
         self.crossroads = []  # Points
         self.calculation_points = {}
         self.plotted_calculation_points = []
@@ -105,28 +109,6 @@ class Hmmm:
                             self.ax.plot(*crossroads.xy,
                                          f"{intersection_p_color}o")
 
-    def add_point_to_road(self, point: Point):
-        """Adds a point to the road that is currently building."""
-        for other_point in self.points:
-            hitbox = create_hitbox(other_point)
-            if point.within(hitbox):
-                if len(self.current_road_points) > 0:
-                    # a road can only use an existing point as a starting point
-                    return
-                else:
-                    # sets an existing point as the starting point of a new road
-                    self.plotted_points[other_point].set_color(
-                        selected_p_color)
-                    self.current_road_points.append(other_point)
-                    return
-
-        self.reset_plotted_point_colors()
-        self.current_road_points.append(point)
-        self.points.append(point)
-        plotted_point = self.ax.plot(*point.xy, f"{normal_p_color}o")[0]
-        self.plotted_points[point] = plotted_point
-        self.ax.plot(*create_hitbox(point).exterior.xy)
-
     def add_calculation_point(self, point: Point):
         """Adds a point that distance is measured from, or to. After adding two points, the next point will remove the previous two.
         Points currently have to be on the same linestring."""
@@ -161,30 +143,81 @@ class Hmmm:
                     print(
                         f"Distance between the two points: {abs(self.calculation_points[1][1] - self.calculation_points[0][1])}")
                 plotted_point = self.ax.plot(
-                    *point.xy, f"{calculation_p_color}o")
+                    *point.xy, f"{calculation_p_color}o")[0]
                 self.plotted_calculation_points.append(plotted_point)
                 return True
         return False
 
+    def check_point_overlap(self, point: Point):
+        """Checks if <point> overlaps with an existing points hitbox, and returns the existing point if so."""
+        for other_point in self.points:
+            hitbox = create_hitbox(other_point)
+            if point.within(hitbox):
+                return other_point
+        return False
+
+    def add_point_to_road(self, point: Point):
+        """Adds a point to the road that is currently building."""
+        self.reset_plotted_point_colors()
+        overlapping_point = self.check_point_overlap(point)
+
+        if not overlapping_point:
+            # create new point normally
+            self.points.append(point)
+            self.current_road_points.append(point)
+            plotted_point = self.ax.plot(*point.xy, f"{normal_p_color}o")[0]
+            self.plotted_points[point] = plotted_point
+            self.ax.plot(*create_hitbox(point).exterior.xy)
+        else:
+            if len(self.current_road_points) > 0:
+                # only the starting point of the new road can be an existing point
+                return
+            # sets an existing point as the starting point of a new road
+            self.plotted_points[overlapping_point].set_color(
+                selected_p_color)
+            self.current_road_points.append(overlapping_point)
+            for road in self.roads:
+                for coord in road.coords:
+                    if overlapping_point.coords[0] == coord:
+                        if coord != road.coords[-1] and coord != road.coords[0]:
+                            print(
+                                "You can only start a new road from the first or last point on the existing road!")
+                            print("Creating new road here.")
+                        else:
+                            # used later to merge these two roads
+                            self.current_road_connects_to = road
+                            print(
+                                "Continuing existing road, will be merged when finished")
+
     def finish_road(self):
-        """Creates new road."""
+        """Creates new road. Also merges it with an existing one if they connect."""
         if len(self.current_road_points) < 2:
             return
         new_road = LineString([(point.x, point.y)
                                for point in self.current_road_points])
-        print(new_road.project(self.points[2]))
+        if self.current_road_connects_to:
+            # join the new and existing roads together, then delete the existing road from roads and the plot too
+            new_road = MultiLineString(
+                [new_road, self.current_road_connects_to])
+            new_road = line_merge(new_road)
+            self.plotted_lines[self.current_road_connects_to].remove()
+            self.roads.remove(self.current_road_connects_to)
+            self.current_road_connects_to = None
         self.roads.append(new_road)
         x, y = new_road.xy
-        self.ax.plot(
-            x, y, label=f"Road {self.counter()}, length {new_road.length}")
+        plotted_line = self.ax.plot(
+            x, y, label=f"Road {self.counter()}, length {new_road.length}")[0]
+        self.plotted_lines[new_road] = plotted_line
         self.current_road_points.clear()
         self.create_crossroads()
         self.create_cursor()
+        print(f"Amount of roads: {len(self.roads)}")
 
     def onclick(self, event):
-        print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-              ('double' if event.dblclick else 'single', event.button,
-               event.x, event.y, event.xdata, event.ydata))
+        if print_click_info:
+            print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+                  ('double' if event.dblclick else 'single', event.button,
+                   event.x, event.y, event.xdata, event.ydata))
         if event.button == MouseButton.RIGHT:
             self.finish_road()
         elif event.button == MouseButton.LEFT:
